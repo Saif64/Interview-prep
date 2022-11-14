633f4d2e74710ffdbb417604c9a0a29ffc619870
Saif64 <saifmahmud81@gmail.com>
2022-11-14 19:56:50 +0600

    added graph

---
+++ Data structures/graphs/graph_data.js
@@ -0,0 +1,26 @@
+/* How to represent a graph data */
+
+// Edge List
+const graph = [
+    [0, 2],
+    [2, 3],
+    [2, 1],
+    [1, 3]
+];
+
+// Aajacent List
+const adjGraph = [
+    [2],
+    [2, 3],
+    [0, 1, 3],
+    [1, 2]
+];
+
+// Adjacent Matrix
+const matGraph = [
+    [0, 0, 1, 0],
+    [0, 0, 1, 1,],
+    [1, 1, 0, 1],
+    [0, 1, 1, 0]
+
+];
