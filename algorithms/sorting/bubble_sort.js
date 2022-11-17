30f9fb9a02bb1bcdc7feba7ec2eef7102e98a514
Saif64 <saifmahmud81@gmail.com>
2022-11-17 13:05:12 +0600

    adding sorting folder

---
+++ algorithms/sorting/bubble_sort.js
@@ -0,0 +1,17 @@
+const arr = [2, 3, 1, 4, 7, 5];
+
+const bubbleSort = (arr) => {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length - 1; j++) {
+            if (arr[j] > arr[j + 1]) {  // arr.sort((a,b) => a - b)  change to arr[j] < arr[j+1] for descending order
+                // swap Number
+                let temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    return arr;
+};
+
+console.log(bubbleSort(arr));   // [1,2,3,4,5,7]
