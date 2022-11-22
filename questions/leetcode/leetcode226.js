bdfa3974463098d5cb2e89864fd895d653d2bcb9
Saif64 <65161732+Saif64@users.noreply.github.com>
2022-11-22 18:52:32 +0600

    invert/mirror a tree

---
+++ questions/leetcode/leetcode226.js
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/invert-binary-tree/description/
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var invertTree = function (root) {
+    if (root) {
+        [root.left, root.right] = [invertTree(root.right), invertTree(root.left)];
+    }
+    return root;
+};
