2d95c639d5a3139283f1cdf4db5bad4b242977e9
Saif64 <65161732+Saif64@users.noreply.github.com>
2022-11-29 14:57:50 +0600

    undirected graph

--- algorithms/searching/graph-theory/undirected_graph/undirected_graph.js
+++ algorithms/searching/graph-theory/undirected_graph/undirected_graph.js
@@ -135,34 +135,34 @@ undirectedPath(edges, 'r', 't'); // -> true
 
 //Depth first search recursive
 const undirectedPath = (edges, nodeA, nodeB) => {
-    const graph = buildGraph(edges);
-    return hasPath(graph, nodeA, nodeB, new Set());
+  const graph = buildGraph(edges);
+  return hasPath(graph, nodeA, nodeB, new Set());
 };
 
 const buildGraph = (edges) => {
-    const graph = {};
+  const graph = {};
 
-    for (let edge of edges) {
-        const [a, b] = edge;
-        if (!(a in graph)) graph[a] = [];
-        if (!(b in graph)) graph[b] = [];
-        graph[a].push(b);
-        graph[b].push(a);
-    }
+  for (let edge of edges) {
+    const [a, b] = edge;
+    if (!(a in graph)) graph[a] = [];
+    if (!(b in graph)) graph[b] = [];
+    graph[a].push(b);
+    graph[b].push(a);
+  }
 
-    return graph;
+  return graph;
 };
 
 const hasPath = (graph, src, dst, visited) => {
-    if (src === dst) return true;
-    if (visited.has(src)) return false;
-    visited.add(src);
+  if (src === dst) return true;
+  if (visited.has(src)) return false;
+  visited.add(src);
 
-    for (let neighbor of graph[src]) {
-        if (hasPath(graph, neighbor, dst, visited) === true) {
-            return true;
-        }
+  for (let neighbor of graph[src]) {
+    if (hasPath(graph, neighbor, dst, visited) === true) {
+      return true;
     }
+  }
 
-    return false;
+  return false;
 };
