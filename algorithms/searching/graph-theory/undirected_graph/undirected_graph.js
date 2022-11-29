4338724830c3b55ebf181fbe30233f6f76abae7e
Saif64 <65161732+Saif64@users.noreply.github.com>
2022-11-29 13:53:12 +0600

    undirected graph

---
+++ algorithms/searching/graph-theory/undirected_graph/undirected_graph.js
@@ -0,0 +1,168 @@
+// Write a function, undirectedPath,
+// that takes in an array of edges for an undirected graph and two nodes(nodeA, nodeB).
+// The function should return a boolean indicating
+// whether or not there exists a path between nodeA and nodeB.;
+
+
+/*
+test_00:
+
+const edges = [
+  ['i', 'j'],
+  ['k', 'i'],
+  ['m', 'k'],
+  ['k', 'l'],
+  ['o', 'n']
+];
+
+undirectedPath(edges, 'j', 'm'); // -> true
+
+test_01:
+
+const edges = [
+  ['i', 'j'],
+  ['k', 'i'],
+  ['m', 'k'],
+  ['k', 'l'],
+  ['o', 'n']
+];
+
+undirectedPath(edges, 'm', 'j'); // -> true
+
+test_02:
+
+const edges = [
+  ['i', 'j'],
+  ['k', 'i'],
+  ['m', 'k'],
+  ['k', 'l'],
+  ['o', 'n']
+];
+
+undirectedPath(edges, 'l', 'j'); // -> true
+
+test_03:
+
+const edges = [
+  ['i', 'j'],
+  ['k', 'i'],
+  ['m', 'k'],
+  ['k', 'l'],
+  ['o', 'n']
+];
+
+undirectedPath(edges, 'k', 'o'); // -> false
+
+test_04:
+
+const edges = [
+  ['i', 'j'],
+  ['k', 'i'],
+  ['m', 'k'],
+  ['k', 'l'],
+  ['o', 'n']
+];
+
+undirectedPath(edges, 'i', 'o'); // -> false
+
+test_05:
+
+const edges = [
+  ['b', 'a'],
+  ['c', 'a'],
+  ['b', 'c'],
+  ['q', 'r'],
+  ['q', 's'],
+  ['q', 'u'],
+  ['q', 't'],
+];
+
+
+undirectedPath(edges, 'a', 'b'); // -> true
+
+test_06:
+
+const edges = [
+  ['b', 'a'],
+  ['c', 'a'],
+  ['b', 'c'],
+  ['q', 'r'],
+  ['q', 's'],
+  ['q', 'u'],
+  ['q', 't'],
+];
+
+undirectedPath(edges, 'a', 'c'); // -> true
+
+test_07:
+
+const edges = [
+  ['b', 'a'],
+  ['c', 'a'],
+  ['b', 'c'],
+  ['q', 'r'],
+  ['q', 's'],
+  ['q', 'u'],
+  ['q', 't'],
+];
+
+undirectedPath(edges, 'r', 't'); // -> true
+
+test_08:
+
+const edges = [
+  ['b', 'a'],
+  ['c', 'a'],
+  ['b', 'c'],
+  ['q', 'r'],
+  ['q', 's'],
+  ['q', 'u'],
+  ['q', 't'],
+];
+
+undirectedPath(edges, 'r', 'b'); // -> false
+
+test_09:
+
+const edges = [
+  ['s', 'r'],
+  ['t', 'q'],
+  ['q', 'r'],
+];
+
+undirectedPath(edges, 'r', 't'); // -> true
+*/
+
+//Depth first search recursive
+const undirectedPath = (edges, nodeA, nodeB) => {
+    const graph = buildGraph(edges);
+    return hasPath(graph, nodeA, nodeB, new Set());
+};
+
+const buildGraph = (edges) => {
+    const graph = {};
+
+    for (let edge of edges) {
+        const [a, b] = edge;
+        if (!(a in graph)) graph[a] = [];
+        if (!(b in graph)) graph[b] = [];
+        graph[a].push(b);
+        graph[b].push(a);
+    }
+
+    return graph;
+};
+
+const hasPath = (graph, src, dst, visited) => {
+    if (src === dst) return true;
+    if (visited.has(src)) return false;
+    visited.add(src);
+
+    for (let neighbor of graph[src]) {
+        if (hasPath(graph, neighbor, dst, visited) === true) {
+            return true;
+        }
+    }
+
+    return false;
+};
