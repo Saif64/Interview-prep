e7b5ed5584a2fd453f37cb54c57a6a199b374786
Saif64 <saifmahmud81@gmail.com>
2023-01-14 12:50:10 +0600

    added leetcode 2367

---
+++ questions/leetcode/leetcode2367.js
@@ -0,0 +1,49 @@
+// https://leetcode.com/problems/number-of-arithmetic-triplets/description/
+
+// initial brute force solution using 3 loops
+/**
+ * @param {number[]} nums
+ * @param {number} diff
+ * @return {number}
+ */
+var arithmeticTriplets = function (nums, diff) {
+    let count = 0;
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            for (let k = j + 1; k < nums.length; k++) {
+                if (nums[j] - nums[i] === diff && nums[k] - nums[j] === diff) {
+                    count++;
+                }
+            }
+        }
+    }
+    return count;
+};
+
+// optimized solution
+var arithmeticTripletsOptimized = function (nums, diff) {
+    let count = 0;
+    for (let num of nums) {
+        if (nums.includes(num + diff) && nums.includes(num + diff * 2)) {
+            count++;
+        }
+    }
+    return count;
+};
+
+var arithmeticTripletsOptimized2 = function (nums, diff) {
+    let count = 0;
+    for (let i = nums.length - 1; i > 0; i--) {
+        if (nums.includes(nums[i] - diff) && nums.includes(nums[i] - diff * 2)) {
+            count++;
+        }
+    }
+    return count;
+};
+
+const nums = [4, 5, 6, 7, 8, 9], diff = 2;
+
+console.log(arithmeticTripletsOptimized2(nums, diff));
+
+
+
