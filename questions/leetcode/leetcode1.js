63c5effa9a469c736e89521e03733f95881fdd9d
Saif64 <saifmahmud81@gmail.com>
2023-01-14 19:47:45 +0600

    modified twoSum a little

--- questions/leetcode/leetcode1.js
+++ questions/leetcode/leetcode1.js
@@ -5,16 +5,35 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function (nums, target) {
-    const map = new Map();
-
+const twoSum = (nums, target) => {
     for (let i = 0; i < nums.length; i++) {
-        const diff = target - nums[i];
-        if (map.has(diff)) {
-            return [i, map.get(diff)];
-        } else {
-            map.set(nums[i], i);
+        const numTofind = target - nums[i];
+        for (let j = i + 1; j < nums.length; j++) {
+            if (numTofind === nums[j]) {
+                return [i, j];
+            }
         }
     }
-    return [];
+    return null;
 };
+
+let arr = [1, 2, 5, 3, 6];
+console.log(twoSum(arr, 8));  // this case fails to find all the solution as 5+3=8 as well
+
+
+// more efficient solution using HashMap
+
+const twoSumHash = (nums, target) => {
+    const map = {};
+    for (let i = 0; i < nums.length; i++) {
+        const currentVal = map[nums[i]];
+        if (currentVal >= 0) return [currentVal, i];
+        else {
+            const numToFind = target - nums[i];
+            map[numToFind] = i;
+        }
+    }
+    return null;
+};
+
+console.log(twoSumHash(arr, 8));
