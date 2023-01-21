8fda8fa4bfe932c680dd80f5abae5ecd9e84a708
Saif64 <saifmahmud81@gmail.com>
2023-01-21 11:13:07 +0600

    leetcode 11 naive solution

--- questions/leetcode/leetcode11.js
+++ questions/leetcode/leetcode11.js
@@ -4,6 +4,22 @@
  * @param {number[]} height
  * @return {number}
  */
+
+// brute force solution
+const maxWater = (arr) => {
+    let maxArea = 0;
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = i + 1; j < arr.length; j++) {
+            let min = Math.min(arr[i], arr[j]) * (j - i);
+            if (maxArea < min) {
+                maxArea = min;
+            }
+        }
+    }
+    return maxArea;
+};
+
+// optimized
 var maxArea = function (height) {
     const len = height.length;
     let L = 0;
