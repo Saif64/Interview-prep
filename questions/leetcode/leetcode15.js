ee11097bc764306b1a2364e8490e557fedd9e20b
Saif64 <saifmahmud81@gmail.com>
2023-01-25 06:39:20 +0600

    3Sum revised

--- questions/leetcode/leetcode15.js
+++ questions/leetcode/leetcode15.js
@@ -7,26 +7,34 @@
  * @return {number[][]}
  */
 var threeSum = function (nums) {
-    let result = [];
     nums.sort((a, b) => a - b);
+    let result = [];
+
     for (let i = 0; i < nums.length - 2; i++) {
         if (i > 0 && nums[i] === nums[i - 1]) continue;
-        let left = i + 1;
-        let right = nums.length - 1;
-        while (left < right) {
-            let sum = nums[i] + nums[left] + nums[right];
-            if (sum === 0) {
-                result.push([nums[i], nums[left], nums[right]]);
-                left++;
-                right--;
-                while (left < right && nums[left] === nums[left - 1]) left++;
-                while (left < right && nums[right] === nums[right + 1]) right--;
-            } else if (sum < 0) {
-                left++;
-            } else {
-                right--;
+
+        let start = i + 1;
+        let end = nums.length - 1;
+
+        while (start < end) {
+            if (nums[i] + nums[start] + nums[end] === 0) {
+                result.push([nums[i], nums[start], nums[end]]);
+
+                while (start < end && nums[start] === nums[start + 1]) {
+                    start++;
+                }
+                start++;
+
+                while (start < end && nums[end] === nums[end - 1]) {
+                    end--;
+                }
+                end--;
             }
+            else if (nums[i] + nums[start] + nums[end] > 0) end--;
+            else start++;
         }
     }
     return result;
 };
+
+console.log(threeSum([-1, 0, 1, 2, -1, 4]));
