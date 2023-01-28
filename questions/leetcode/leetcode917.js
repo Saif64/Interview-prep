4632465274494b01447f7a1ec35be8be321d32f2
Saif64 <saifmahmud81@gmail.com>
2023-01-28 20:57:48 +0600

    leetcode 917 added

---
+++ questions/leetcode/leetcode917.js
@@ -0,0 +1,26 @@
+// (A <= char <= Z) || (a <= char <= z)
+var isLetter = function (c) {
+    return (c >= 65 && c <= 90) || (c >= 97 && c <= 122);
+};
+
+var swap = function (array, a, b) {
+    [array[a], array[b]] = [array[b], array[a]];
+};
+
+var reverseOnlyLetters = function (S) {
+    var result = S.split('');
+    let l = 0;
+    let r = result.length - 1;
+
+    while (l < r) {
+        if (!isLetter(result[l].charCodeAt())) {
+            l++;
+        } else if (!isLetter(result[r].charCodeAt())) {
+            r--;
+        } else {
+            swap(result, l++, r--);
+        }
+    }
+
+    return result.join('');
+};
