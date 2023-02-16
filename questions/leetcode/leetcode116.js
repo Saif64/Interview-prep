6a3f0156dd9831a762b23d89b390f4535c4900ae
Saif64 <saifmahmud81@gmail.com>
2023-02-16 14:39:02 +0600

    minor change in leetcode116

--- questions/leetcode/leetcode116.js
+++ questions/leetcode/leetcode116.js
@@ -16,6 +16,7 @@ var connect = function (root) {
     let ptr = root;
     while (root && root.left) {
         let temp = root;
+
         while (temp) {
             temp.left.next = temp.right;
             temp.right.next = temp.next ? temp.next.left : null;
