c30cd9323f4a2ca6bfdaf30cb9a63cd8ed73c153
Saif64 <saifmahmud81@gmail.com>
2023-02-16 09:55:50 +0600

    leetcode 116

---
+++ questions/leetcode/leetcode116.js
@@ -0,0 +1,27 @@
+/**
+ * // Definition for a Node.
+ * function Node(val, left, right, next) {
+ *    this.val = val === undefined ? null : val;
+ *    this.left = left === undefined ? null : left;
+ *    this.right = right === undefined ? null : right;
+ *    this.next = next === undefined ? null : next;
+ * };
+ */
+
+/**
+ * @param {Node} root
+ * @return {Node}
+ */
+var connect = function (root) {
+    let ptr = root;
+    while (root && root.left) {
+        let temp = root;
+        while (temp) {
+            temp.left.next = temp.right;
+            temp.right.next = temp.next ? temp.next.left : null;
+            temp = temp.next;
+        }
+        root = root.left;
+    }
+    return ptr;
+};
