ed0483d879a3dfec29dfa4dec260cef1b7ee652a
Saif64 <saifmahmud81@gmail.com>
2023-02-20 11:48:17 +0600

    approach leetcode171

--- questions/leetcode/leetcode171.js
+++ questions/leetcode/leetcode171.js
@@ -10,3 +10,9 @@ var titleToNumber = function (columnTitle) {
     }
     return result;
 };
+
+
+/*
+Very simple charCode of a upper case character - 64 = their alphabet position and 26 pow position of the character (starting from right) - so length - (i+1) means that the last character will end up being just it's alphabet position (character * 1), the one before is going to be * 26, the one before * 26 * 26 and so on.
+Simple math solution that works just fine.
+*/
